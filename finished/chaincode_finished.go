bde0b6d3f157729b84a490b79b88590a736acf7b
gaurangtorvekar <gaurangtorvekar@gmail.com>
2017-04-02 16:04:07 +0800

    Modified the chaincode acc to v1.0

--- finished/chaincode_finished.go
+++ finished/chaincode_finished.go
@@ -1,20 +1,127 @@
 package main
 
 import (
-	"errors"
 	"fmt"
-	s "strings"
+	// "strconv"
 	"encoding/json"
+	s "strings"
 
 	"github.com/hyperledger/fabric/core/chaincode/shim"
+	pb "github.com/hyperledger/fabric/protos/peer"
 )
 
 // SimpleChaincode example simple Chaincode implementation
 type SimpleChaincode struct {
 }
 type Signature struct {
-	Emails string `json:"email"`
+	Emails  string `json:"email"`
 	PdfHash string `json:"pdfhash"`
+}
+
+func (t *SimpleChaincode) Init(stub shim.ChaincodeStubInterface) pb.Response {
+	fmt.Println("########### example_cc Init ###########")
+	_, args := stub.GetFunctionAndParameters()
+	var err error
+
+	if len(args) != 1 {
+		return shim.Error("Incorrect number of arguments. Expecting 1")
+	}
+
+	// Write the state to the ledger
+	err = stub.PutState("initialize_var", []byte(args[0]))
+	if err != nil {
+		return shim.Error(err.Error())
+	}
+
+	return shim.Success(nil)
+
+}
+
+func (t *SimpleChaincode) Query(stub shim.ChaincodeStubInterface) pb.Response {
+	return shim.Error("Unknown supported call")
+}
+
+// Transaction makes payment of X units from A to B
+func (t *SimpleChaincode) Invoke(stub shim.ChaincodeStubInterface) pb.Response {
+	fmt.Println("invoke is running ")
+	function, args := stub.GetFunctionAndParameters()
+
+	if function != "invoke" {
+		return shim.Error("Unknown function call")
+	}
+
+	if len(args) < 2 {
+		return shim.Error("Incorrect number of arguments. Expecting at least 2")
+	}
+
+	if args[0] == "query" {
+		// queries an entity state
+		return t.query(stub, args)
+	}
+	if args[0] == "write" {
+		// Adds a new signature to the state
+		return t.write(stub, args)
+	}
+	return shim.Error("Unknown action, check the first argument, must be one of 'delete', 'query', or 'move'")
+}
+
+func (t *SimpleChaincode) write(stub shim.ChaincodeStubInterface, args []string) pb.Response {
+	var email, hash string
+	var err error
+	fmt.Println("running write()")
+
+	if len(args) != 2 {
+		return shim.Error("Incorrect number of arguments. Expecting 2!")
+	}
+
+	email = args[1] //rename for funsies
+	hash = args[0]
+
+	// Check if PDF has already been signed by this user
+	sigAsBytes, err := stub.GetState(hash)
+	if err != nil {
+		return shim.Error("Failed to get signature hash")
+	}
+	res := Signature{}
+	json.Unmarshal(sigAsBytes, &res)
+	old_emails_str := res.Emails
+	old_emails := s.Split(old_emails_str, ",")
+	new_emails := []string{}
+	new_emails = append(new_emails, email)
+	if res.PdfHash == hash {
+		for _, v := range old_emails {
+			if v != email {
+				new_emails = append(new_emails, v)
+			}
+		}
+	}
+	new_emails_string := s.Join(new_emails, ",")
+	// build the signatures json string manually
+	str := `{"email": "` + new_emails_string + `", "pdfhash": "` + hash + `"}`
+	err = stub.PutState(hash, []byte(str))
+	if err != nil {
+		return shim.Error(err.Error())
+	}
+
+	return shim.Success(nil)
+}
+
+// Query callback representing the query of a chaincode
+func (t *SimpleChaincode) query(stub shim.ChaincodeStubInterface, args []string) pb.Response {
+	var key string
+	var err error
+
+	if len(args) != 1 {
+		return shim.Error("Incorrect number of arguments. Expecting name of the key to query")
+	}
+
+	key = args[0]
+	valAsbytes, err := stub.GetState(key)
+	if err != nil {
+		jsonResp := "{\"Error\":\"Failed to get state for " + key + "\"}"
+		return shim.Error(jsonResp)
+	}
+	return shim.Success(valAsbytes)
 }
 
 func main() {
@@ -22,106 +129,4 @@ func main() {
 	if err != nil {
 		fmt.Printf("Error starting Simple chaincode: %s", err)
 	}
-}
-
-// Init resets all the things
-func (t *SimpleChaincode) Init(stub shim.ChaincodeStubInterface, function string, args []string) ([]byte, error) {
-	if len(args) != 1 {
-		return nil, errors.New("Incorrect number of arguments. Expecting 1")
-	}
-
-	err := stub.PutState("initialize_var", []byte(args[0]))
-	if err != nil {
-		return nil, err
-	}
-
-	return nil, nil
-}
-
-// Invoke isur entry point to invoke a chaincode function
-func (t *SimpleChaincode) Invoke(stub shim.ChaincodeStubInterface, function string, args []string) ([]byte, error) {
-	fmt.Println("invoke is running " + function)
-
-	// Handle different functions
-	if function == "init" {
-		return t.Init(stub, "init", args)
-	} else if function == "write" {
-		return t.write(stub, args)
-	}
-	fmt.Println("invoke did not find func: " + function)
-
-	return nil, errors.New("Received unknown function invocation: " + function)
-}
-
-// Query is our entry point for queries
-func (t *SimpleChaincode) Query(stub shim.ChaincodeStubInterface, function string, args []string) ([]byte, error) {
-	fmt.Println("query is running " + function)
-
-	// Handle different functions
-	if function == "read" { //read a variable
-		return t.read(stub, args)
-	}
-	fmt.Println("query did not find func: " + function)
-
-	return nil, errors.New("Received unknown function query: " + function)
-}
-
-// write - invoke function to write key/value pair
-func (t *SimpleChaincode) write(stub shim.ChaincodeStubInterface, args []string) ([]byte, error) {
-    var email, hash string
-    var err error
-    fmt.Println("running write()")
-
-    if len(args) != 2 {
-        return nil, errors.New("Incorrect number of arguments. Expecting 2. name of the key and value to set")
-    }
-    email = args[1] //rename for funsies
-    hash = args[0]
-
-    // Check if PDF has already been signed by this user
-    sigAsBytes, err := stub.GetState(hash)
-    if err != nil {
-        return nil, errors.New("Failed to get signature hash")
-    }   
-    res := Signature{}
-    json.Unmarshal(sigAsBytes, &res)
-    old_emails_str := res.Emails
-    old_emails := s.Split(old_emails_str, ",")
-	new_emails := []string{}
-	new_emails = append(new_emails, email)
-    if res.PdfHash == hash {
-    	for _, v := range old_emails {
-		    if v != email {
-		        new_emails = append(new_emails, v)
-		    }
-		}
-    }
-    new_emails_string := s.Join(new_emails, ",")
-    // build the signatures json string manually
-    str := `{"email": "` + new_emails_string + `", "pdfhash": "` + hash + `"}`
-    err = stub.PutState(hash, []byte(str))
-    if err != nil {
-        return nil, err
-    }
-
-    return nil, nil
-}
-
-// read - query function to read key/value pair
-func (t *SimpleChaincode) read(stub shim.ChaincodeStubInterface, args []string) ([]byte, error) {
-	var key, jsonResp string
-	var err error
-
-	if len(args) != 1 {
-		return nil, errors.New("Incorrect number of arguments. Expecting name of the key to query")
-	}
-
-	key = args[0]
-	valAsbytes, err := stub.GetState(key)
-	if err != nil {
-		jsonResp = "{\"Error\":\"Failed to get state for " + key + "\"}"
-		return nil, errors.New(jsonResp)
-	}
-
-	return valAsbytes, nil
 }
